59c69587290814f516f4a8f45d39e08f2d6e1c1a
Farhan Abdulkadir <abdifarhan41@gmail.com>
2024-08-23 23:51:53 -0500

    function basket done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,13 +1,88 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+// Declare global variable named basked and set it to empty 
+let basket= [];
+
+// function add an item to the basket 
+function addItem(item){
+    // add item to the basket array
+    basket.push(item);
+    // return true if item was added
+    return true
+
+
+}
 
 
 
+function listItems(){
+
+
+    for(let i=0; i<basket.length; i++){
+        console.log(basket[i]);
+    }
+
+}
+
+
+function empty(){
+
+    basket.splice(0, basket.length);
+    return basket;
 
 
 
+}
 
+
+// testing the function 
+
+// test addItem function
+
+console.log(`Basket is ${basket}`);
+console.log('Adding Apples(expect true)', addItem('Apple'));
+console.log('Adding orange(expect true)', addItem('Orange'));
+console.log('Adding grape(expect true)', addItem('Grape'));
+console.log('Adding banana (expect true)', addItem('Banana'));
+console.log(`Basket is now ${basket}`);
+
+// test listItem function
+
+console.log('listing items in basket');
+listItems();
+
+
+
+// test empty function 
+
+console.log('emptying basket...');
+console.log(empty());
+
+// STRETCH GOALS 
+
+// declare global variable named maxItems and s et it to 5
+
+const maxItems = 5;
+
+function isFull(){
+// if the items in the basket aren't full return true
+
+    if(basket.length>=maxItems){
+        return true;
+    } else {
+        return false;
+    }
+
+
+}
+
+
+// test isFull function 
+
+console.log(`is the function full? ${isFull()}`);
+
+// updated addItem function 
 
 
 
